d3e4305b3a9c8f8941689051c6bcf8c38b98f16e
Matheus Duarte <matheusduartegalvao@gmail.com>
2023-10-12 16:52:50 -0300

    feat: addind update and delete invoices routes

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -28,9 +28,9 @@ class InvoiceController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'user_id'      => 'required',
-            'type'         => 'required|max:1',
+            'type'         => 'required|max:1|in:' . implode(',', ['B','P','C']),
             'paid'         => 'required|numeric|between:0,1',
-            'payment_date' => 'mullable',
+            'payment_date' => 'nullable',
             'value'        => 'required|numeric|between:1,9999.99'
         ]);
 
@@ -58,16 +58,48 @@ class InvoiceController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Invoice $invoice)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id'      => 'required',
+            'type'         => 'required|max:1|in:' . implode(',', ['B','P','C']),
+            'paid'         => 'required|numeric|between:0,1',
+            'payment_date' => 'nullable|date_format:Y-m-d H:i:s',
+            'value'        => 'required|numeric'
+        ]);
+
+        if($validator->fails()) {
+            return $this->error('Validation failed', 422, $validator->errors());
+        }
+
+        $validated = $validator->validated();
+
+        $updated = $invoice->update([
+            'user_id'      => $validated['user_id'],
+            'type'         => $validated['type'],
+            'paid'         => $validated['paid'],
+            'value'        => $validated['value'],
+            'payment_date' => $validated['paid'] ? $validated['payment_date'] : null
+        ]);
+
+        if($updated) {
+            return $this->response('Invoice updated', 200, new InvoiceResource($invoice->load('user')));
+        }
+
+        return $this->error("Invoice not updated", 400);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Invoice $invoice)
     {
-        //
+        $deleted = $invoice->delete();
+
+        if($deleted) {
+            return $this->response('Invoiced deleted', 200);
+        }
+
+        return $this->error('Invoiced not deleted', 400);
     }
 }
